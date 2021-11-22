93735259066353d959c2967ee788ef5c60730431
19860917013 <913552312@qq.com>
2021-11-22 21:48:03 +0800

    实现接口文档

--- imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/HelloController.java
+++ imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.imooc.controller;
 
 import com.imooc.grace.result.GraceJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/11/22 19 :41
  * @description
  **/
-
+@Slf4j
+@Api(tags = "Hello 测试的接口")
 @RestController
 public class HelloController {
+
+    @ApiOperation(value = "hello-这是一个hello的测试路由")
     @GetMapping("/hello")
     public Object hello() {
         return GraceJSONResult.ok("Hello ,SpringBoot");
