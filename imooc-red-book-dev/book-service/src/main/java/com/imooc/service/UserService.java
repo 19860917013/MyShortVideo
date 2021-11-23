49a91cbce11812ade60fb3eb056855637434cecd
19860917013 <913552312@qq.com>
2021-11-23 22:57:21 +0800

    创建用户的Service

--- imooc-red-book-dev/book-service/src/main/java/com/imooc/service/UserService.java
+++ imooc-red-book-dev/book-service/src/main/java/com/imooc/service/UserService.java
@@ -13,5 +13,11 @@ public interface UserService {
      * 判断用户是否存在，如果存在返回user信息
      */
     public Users queryMobileIsExist(String mobile);
+
+    /**
+     * 创建用户，新增用户记录到数据库
+     */
+    public Users createUser(String mobile);
+
 }
 
--- imooc-red-book-dev/book-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ imooc-red-book-dev/book-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,11 +1,19 @@
 package com.imooc.service.impl;
 
+import com.imooc.enums.Sex;
+import com.imooc.enums.YesOrNo;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
 import com.imooc.service.UserService;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.DesensitizationUtil;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
 
 /**
  * @author 包建丰
@@ -28,4 +36,45 @@ public class UserServiceImpl implements UserService {
         Users user = usersMapper.selectOneByExample(userExample);
         return user;
     }
+
+    @Autowired
+    public Sid sid;
+    private static final String USER_FACE1 = "http://122.152.205.72:88/group1/M00/00/05/CpoxxF6ZUySASMbOAABBAXhjY0Y649.png";
+
+    @Transactional
+    @Override
+    public Users createUser(String mobile) {
+        /**
+         * 互联网项目都要考虑可扩展性
+         * 如果未来的业务激增，那么就需要分库分表
+         * 那么数据库表主键id必须保证全局（全库）唯一，不得重复
+         */
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+
+        user.setId(userId);
+        user.setMobile(mobile);
+        user.setNickname("用户：" + DesensitizationUtil.commonDisplay(mobile));
+        user.setImoocNum("用户：" + DesensitizationUtil.commonDisplay(mobile));
+        user.setFace(USER_FACE1);
+
+        user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+        user.setSex(Sex.secret.type);
+
+        user.setCountry("中国");
+        user.setProvince("");
+        user.setCity("");
+        user.setDistrict("");
+        user.setDescription("这家伙很懒，什么都没留下~");
+        user.setCanImoocNumBeUpdated(YesOrNo.YES.type);
+
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        usersMapper.insert(user);
+
+        return user;
+    }
+
 }
