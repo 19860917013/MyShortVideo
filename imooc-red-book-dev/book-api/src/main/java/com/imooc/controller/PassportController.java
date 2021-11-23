0327e6428c614ae2912726ef235ff8210e5b68c4
19860917013 <913552312@qq.com>
2021-11-23 12:32:35 +0800

    完善发送短信接口

---
+++ imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/PassportController.java
@@ -0,0 +1,51 @@
+package com.imooc.controller;
+
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.IPUtil;
+import com.imooc.utils.SMSUtils;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author 包建丰
+ * @date 2021/11/23 12 :20
+ * @description
+ **/
+
+@Api(tags = "PassportController 通行证接口模块")
+@RestController
+@RequestMapping("passport")
+@Slf4j
+public class PassportController {
+
+    @Autowired
+    private SMSUtils smsUtils;
+
+    @PostMapping("getSMSCode")
+    public GraceJSONResult getSMSCode(@RequestParam String mobile,
+                                      HttpServletRequest request) throws Exception {
+        if (StringUtils.isBlank(mobile)) {
+            return GraceJSONResult.ok();
+        }
+
+        // TODO 获得用户ip，
+        String userIp = IPUtil.getRequestIp(request);
+        // TODO 根据用户ip进行限制，限制用户在60秒之内只能获得一次验证码
+
+        String code = (int) ((Math.random() * 9 + 1) * 100000) + "";
+        smsUtils.sendSMS("19860917013", code);
+        log.info(code);
+
+        // TODO 把验证码放入到redis中，用于后续的验证
+
+        return GraceJSONResult.ok();
+    }
+}
