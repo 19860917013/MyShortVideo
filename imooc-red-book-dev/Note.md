3c2ecdbd9b580e2bcc5bb0f9307c8cf51ae29bd2
19860917013 <913552312@qq.com>
2021-11-23 12:17:37 +0800

    SpringBoot 集成腾讯云短信

---
+++ imooc-red-book-dev/Note.md
@@ -0,0 +1,7 @@
+# SecretId:
+
+    AKIDSii02nHYfdNr52j8IBn8ByUifMmulMgj
+
+# SecretKey:
+
+    nJYXEeoiKfkY4JHxMULwGI3tSjjSv60d
--- imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/HelloController.java
+++ imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.imooc.controller;
 
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.SMSUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +24,17 @@ public class HelloController {
     public Object hello() {
         return GraceJSONResult.ok("Hello ,SpringBoot");
     }
+
+    @Autowired
+    private SMSUtils smsUtils;
+
+    @GetMapping("sms")
+    public Object sms() throws Exception {
+
+        String code = "123456";
+        smsUtils.sendSMS("19860917013", code);
+
+        return GraceJSONResult.ok();
+    }
+
 }
