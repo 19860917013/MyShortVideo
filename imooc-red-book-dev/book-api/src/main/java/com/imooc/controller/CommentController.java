a8c92527f151358ef87baa4618083e77188b1fd2
19860917013 <913552312@qq.com>
2021-11-25 22:39:46 +0800

    评论总数的显示

--- imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/CommentController.java
+++ imooc-red-book-dev/book-api/src/main/java/com/imooc/controller/CommentController.java
@@ -1,16 +1,16 @@
 package com.imooc.controller;
 
+import com.imooc.base.BaseInfoProperties;
 import com.imooc.bo.CommentBO;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.service.CommentService;
 import com.imooc.vo.CommentVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 @Api(tags = "CommentController 评论模块的接口")
 @RequestMapping("comment")
 @RestController
-public class CommentController {
+public class CommentController extends BaseInfoProperties {
     @Autowired
     private CommentService commentService;
 
@@ -31,14 +31,14 @@ public class CommentController {
     public GraceJSONResult create(@RequestBody @Valid CommentBO commentBO) {
 
         /**
-         * TODO: 作业
+         * TODO:
          * fatherCommentId不为空且不为“0”的时候，
          * 校验上一条评论是否真实存在，不存在抛出异常
          */
 
 
         /**
-         * TODO: 作业
+         * TODO:
          * vlogerId 和 commentUserId 都是对应用户的主键，
          * 需要校验是否真实存在用户，不存在抛出异常
          * 进阶：封装统一方法专门用于校验，[ checkUserExist(userId) ]
@@ -48,6 +48,18 @@ public class CommentController {
         CommentVO commentVO = commentService.createComment(commentBO);
 
         return GraceJSONResult.ok(commentVO);
+    }
+
+
+    @ApiOperation(value = "短视频的评论总数")
+    @GetMapping("counts")
+    public GraceJSONResult counts(@RequestParam String vlogId) {
+
+        String countsStr = redis.get(REDIS_VLOG_COMMENT_COUNTS + ":" + vlogId);
+        if (StringUtils.isBlank(countsStr)) {
+            countsStr = "0";
+        }
+        return GraceJSONResult.ok(Integer.valueOf(countsStr));
     }
 
 }
